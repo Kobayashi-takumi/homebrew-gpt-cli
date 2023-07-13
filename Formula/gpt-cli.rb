6769f991a9c2245a414173ff5eff7e8a6ec3d618
Takumi Kobayashi <t.kobayashi@inside.tworks.co.jp>
2023-07-13 16:09:52 +0900

    v0.2.0

--- Formula/gpt-cli.rb
+++ Formula/gpt-cli.rb
@@ -3,7 +3,7 @@ class GptCli < Formula
   homepage "https://github.com/Kobayashi-takumi/gpt-cli"
   url "https://github.com/Kobayashi-takumi/gpt-cli/releases/download/v0.2.0/gpt-cli-0.2.0-aarch64-apple-darwin.tar.gz"
   sha256 "a1d5b96d95fe11648d2f69ba723379f9ae444386ba68ef3888fcc77a8dc3a365"
-  version "0.1.0"
+  version "0.2.0"
 
   def install
     bin.install "gpt-cli"
