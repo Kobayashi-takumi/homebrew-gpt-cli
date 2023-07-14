75a0189070a6fb82b5a5a264bbcd1a43bd595934
Takumi Kobayashi <t.kobayashi@inside.tworks.co.jp>
2023-07-14 16:51:12 +0900

    v0.4.0

--- Formula/gpt-cli.rb
+++ Formula/gpt-cli.rb
@@ -1,8 +1,8 @@
 class GptCli < Formula
   desc "Communicate with ChatGPT from the CLI."
   homepage "https://github.com/Kobayashi-takumi/gpt-cli"
-  url "https://github.com/Kobayashi-takumi/gpt-cli/releases/download/v0.4.0/gpt-0.4.0-aarch64-apple-darwin.tar.gz"
-  sha256 "dd1da5cf96ebfd3553cb086558e3a534248d997bfde4bc594d1ae71d6a12fb38"
+  url "https://github.com/Kobayashi-takumi/gpt-cli/releases/download/v0.4.0/gpt-cli-0.4.0-aarch64-apple-darwin.tar.gz"
+  sha256 "1df70ac19eed800cb42102176b1f77c9b582bffdadfa806d07ec3646dc9e6e71"
   version "0.4.0"
 
   def install
